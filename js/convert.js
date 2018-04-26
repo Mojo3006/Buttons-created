b726af77d33abb78d65f181ada38331948ec3e88
Mojo3006 <35809331+Mojo3006@users.noreply.github.com>
2018-04-26 16:20:08 +0100

    Add files via upload

---
+++ js/convert.js
@@ -0,0 +1,26 @@
+function fToM(){
+
+var measure = parseInt(document.getElementById("value1").value);
+
+
+
+//Conversions
+var meters = measure * 0.3048;
+var centimetres = measure * 2.54
+var yardstometers = measure * 0.9144
+var Kilometers = measure * 1.60934
+
+//Imperial to Metric
+var message = measure + ' feet converts to ' + meters + ' meters.';
+var message = measure + ' inches converts to' + centimeters + ' centimeters.';
+var message = measure + ' yards converts to' + meters + ' meters.';
+var message = measure + ' miles converts to' + kilometers + ' kilometers.';
+  
+  
+  console.flag(message);
+  document.getElementbyId("Metersresultsentence").innerHTML = message;
+  document.getElementById("Centimetresresultsentence").innerHTML = message;
+  document.getElementbyId("Metersresultsentence").innerHTML = message;
+  document.getElementById("kilometersresultsentence").innerHTML = message;
+
+}
